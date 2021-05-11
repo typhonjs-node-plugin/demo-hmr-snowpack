2302480030912ba1b81fd7df3b28a5bfbc030cc6
typhonrt <support@typhonjs.io>
2021-05-11 03:44:30 -0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -14,21 +14,25 @@ import PluginManager  from '@typhonjs-plugin/manager';
 
 const pluginManager = new PluginManager();
 
-// Load the plugins by target from absolute paths. During Snowpack runtime `./src` becomes `./dist`.
-pluginManager.add({ name: 'Plugin', target: '/dist/Plugin.js' });
+// The plugin manager is asynchronous so for proper functioning needs to use an async iife, top level await, or invoked
+// from an async function. The plugin manager uses import dynamic.
+(async () => {
+  // Load the plugins by target from absolute paths. During Snowpack runtime `./src` becomes `./dist`.
+  await pluginManager.add({ name: 'Plugin', target: '/dist/Plugin.js' });
 
-// To load a relative path you need to construct an URL w/ import.meta.url
-pluginManager.add({ name: 'Plugin2', target: new URL('Plugin2.js', import.meta.url) });
+  // To load a relative path you need to construct an URL w/ import.meta.url
+  await pluginManager.add({ name: 'Plugin2', target: new URL('Plugin2.js', import.meta.url) });
 
 
-// The following plugins are empty implementations showing the bare minimum requirements for a default instantiated
-// class and bare module.
+  // The following plugins are empty implementations showing the bare minimum requirements for a default instantiated
+  // class and bare module.
 
-// Shows how to load from URL from the hostname / port defined above.
-pluginManager.add({ name: 'PluginSimple', target: new URL('PluginSimple.js', import.meta.url) });
+  // Shows how to load from URL from the hostname / port defined above.
+  await pluginManager.add({ name: 'PluginSimple', target: new URL('PluginSimple.js', import.meta.url) });
 
-// Shows how to load from URL from the hostname / port defined above.
-pluginManager.add({ name: 'PluginModule', target: new URL('PluginModule.js', import.meta.url) });
+  // Shows how to load from URL from the hostname / port defined above.
+  await pluginManager.add({ name: 'PluginModule', target: new URL('PluginModule.js', import.meta.url) });
+})();
 
 // Confetti launching from original base demo.
 confetti.create(document.getElementById('canvas'), {
